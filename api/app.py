b6e4df3bf5e1d95164f51dc8ba06122c6fffde68
19bk <bernardkibathi91@gmail.com>
2023-10-23 11:16:42 +0300

    modified:   .gitignore 	new file:   Dockerfile 	modified:   api/app.py 	modified:   api/requirements.txt 	new file:   docker-compose.yml

--- api/app.py
+++ api/app.py
@@ -1,22 +1,37 @@
 from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb://db:27017/')  # 'db' is the name of the MongoDB service in docker-compose.yml
+db = client.mydb  # 'mydb' is the name of the database
+collection = db.my_collection  # Choose a collection name
 
-@app.route('/receive-alert', methods=['POST'])
-def receive_alert():
-    try:
-        data = request.json
-        memory_usage = data.get('memoryUsage')
-        
-        if memory_usage is not None:
-            print(f"Received alert! Memory usage: {memory_usage}%")
-            return jsonify({'message': 'Alert received', 'memoryUsage': memory_usage}), 200
-        else:
-            return jsonify({'message': 'Invalid data', 'error': 'memoryUsage not found'}), 400
-            
-    except Exception as e:
-        print(e)
-        return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
+@app.route('/value', methods=['GET', 'POST', 'PUT'])
+def value():
+    if request.method == 'POST':
+        key = request.json.get('key')
+        value = request.json.get('value')
+        if key and value:
+            collection.insert_one({'key': key, 'value': value})
+            return jsonify({'message': 'Value created'}), 201
+        return jsonify({'message': 'Invalid request'}), 400
+
+    elif request.method == 'PUT':
+        key = request.json.get('key')
+        new_value = request.json.get('new_value')
+        if key and new_value:
+            collection.update_one({'key': key}, {'$set': {'value': new_value}})
+            return jsonify({'message': 'Value updated'}), 200
+        return jsonify({'message': 'Invalid request'}), 400
+
+    elif request.method == 'GET':
+        key = request.args.get('key')
+        if key:
+            item = collection.find_one({'key': key})
+            if item:
+                return jsonify({'value': item['value']}), 200
+            return jsonify({'message': 'Key not found'}), 404
+        return jsonify({'message': 'Invalid request'}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
